3d4ea4a3daec7f4adac2dd9dd13e4e0a10285b2a
JereMIbq1995 <duo13001@byui.edu>
2021-03-17 17:54:58 -0600

    Added buffer and point class

---
+++ game/buffer.py
@@ -0,0 +1,55 @@
+from actor import Actor
+from point import Point
+import constants
+
+class Buffer(Actor):
+    def __init__(self):
+        """
+        Initialize the buffer
+        """
+        super().__init__()
+        self._text = ""
+        self.set_position(Point(1,constants.MAX_Y))
+    
+    def push(self, char):
+        """
+        Adds the given character to the end of the buffer
+        """
+        self._text += char
+    
+    def pop(self):
+        """
+        Removes the last character from the end of the buffer
+        """
+        self._text = self._text[:-1]
+    
+    def toString(self):
+        """
+        Return the buffer string
+        """
+        return self._text
+    
+    def clearBuffer(self):
+        """
+        Clear the buffer
+        """
+        self._text = ""
+    
+    def updateBuffer(self, char):
+        """
+        - If the char is in the alphabet, add the char to the buffer
+        - If the string "backspace" is received, then simply call pop()
+        - If the '*' character is received, clear the buffer
+        """
+        if (len(char) == 1 and ord(char) >= 97 and ord(char) <= 122):
+            self.push(char)
+        elif (char == "backspace"):
+            self.pop()
+        elif (char == "*"):
+            self.clearBuffer()
+
+    def isInBuffer(self, substring):
+        """
+        Check to see if the given substring is part of the buffer
+        """
+        return False if self._text.find(substring) == -1 else True
